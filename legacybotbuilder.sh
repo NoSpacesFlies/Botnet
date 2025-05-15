454ab15b0a93ed300100082de23ed9e7f3ca5fd9
NoSpacesFlies <126734507+NoSpacesFlies@users.noreply.github.com>
2025-05-15 20:42:19 +0300

    Update legacybotbuilder.sh

--- legacybotbuilder.sh
+++ legacybotbuilder.sh
@@ -1,6 +1,18 @@
 ufw disable
 cd bot
+apt upgrade
 apt update -y
+
+sudo iptables -F
+sudo iptables -X
+sudo iptables -t nat -F
+sudo iptables -t nat -X
+sudo iptables -t mangle -F
+sudo iptables -t mangle -X
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+
 
 #setup http server if not installed
        
