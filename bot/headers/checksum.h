e0846633799dba08242cd88de10438e1e03e5621
NoSpacesFlies <126734507+NoSpacesFlies@users.noreply.github.com>
2025-05-24 17:09:58 +0300

    Update checksum.h

--- bot/headers/checksum.h
+++ bot/headers/checksum.h
@@ -3,9 +3,10 @@
 #ifndef CHECKSUM_H
 #define CHECKSUM_H
 #include <stddef.h>
-#include <netinet/in.h>
+#include <stdint.h>
+#include <arpa/inet.h>
 
 unsigned short generic_checksum(void* b, int len);
-unsigned short tcp_udp_checksum(const void* buff, size_t len, in_addr_t src_addr, in_addr_t dest_addr, unsigned char proto);
+unsigned short tcp_udp_checksum(const void* buff, size_t len, unsigned int src_addr, unsigned int dest_addr, unsigned char proto);
 
 #endif // CHECKSUM_H
