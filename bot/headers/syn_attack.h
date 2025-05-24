17b28752b3ddd387e28d45479bda5a01a72636f5
NoSpacesFlies <126734507+NoSpacesFlies@users.noreply.github.com>
2025-05-24 17:09:28 +0300

    Update syn_attack.h

--- bot/headers/syn_attack.h
+++ bot/headers/syn_attack.h
@@ -4,8 +4,8 @@
 #define SYN_ATTACK_H
 
 #include <arpa/inet.h>
-#include <netinet/ip.h>
-#include <netinet/tcp.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
 #include "attack_params.h"
 
 void* syn_attack(void* arg);
